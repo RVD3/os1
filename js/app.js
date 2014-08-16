fe779fd2e2318d20dc2941cf17f024c9889f615a
Ryan Do <ryando33@gmail.com>
2014-08-16 19:24:26 -0400

    made initial design changes and added modal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(function() {
+    
+    $('.modalLink').on('click', function(e) {
+        e.preventDefault();
+        
+        $('#modal, #modalBg').slideDown();
+    });
+    
+    $('#modalBg, #modalClose').on('click', function(e) {
+        e.preventDefault();
+        
+        $('#modal, #modalBg').fadeOut();
+    });
+    
+});
